aba1a2870337ac9961cb57d8fa7fdd7c8cd7e7d0
Ahsan <ahsanan@outlook.com>
2026-05-22 19:53:28 +0600

    stable postgres image for docker

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello from first Docker container");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('✅ Hello from my first Dockerized Full-Stack App!\nIt is working! 🎉\n');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
